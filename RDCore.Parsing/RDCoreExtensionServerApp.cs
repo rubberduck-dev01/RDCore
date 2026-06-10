b15d5e855264a7b1dbda489d6b0ee0d7a4c57595
Mathieu Guindon <mathieu.guindon@dev.rubberduckvba.ca>
2026-06-10 11:17:43 -0400

    Fix main

--- RDCore.Parsing/RDCoreExtensionServerApp.cs
+++ RDCore.Parsing/RDCoreExtensionServerApp.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Logging;
+using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
+using OmniSharp.Extensions.LanguageServer.Protocol.Server;
 using RDCore.SDK.Server;
+using RDCore.SDK.Server.Services;
+using RDCore.SDK.Server.Services.States;
 
 namespace RDCore.Parsing;
 
-internal class RDCoreExtensionServerApp : ServerApp
+internal class RDCoreExtensionServerApp(
+    IServerStateProvider serverStateProvider,
+    IHealthCheckService<ILanguageServerApp> healthCheckService,
+    ILanguageServerProtocolTransportLayer transportLayer,
+    ILogger<RDCoreExtensionServerApp> logger)
+    : LanguageServerApp(serverStateProvider, healthCheckService, transportLayer, logger)
 {
-    protected override void ConfigureAppServices(IServiceCollection services)
+    protected override void ConfigureHandlers(IRDCoreLSPHandlerConfigurationBuilder builder)
     {
+        // TODO
     }
+
+    protected override async Task RegisterServerCapabilitiesAsync(ILanguageServer server, ClientCapabilities clientCapabilities, CancellationToken token)
+    {
+        // TODO
+    }
+
+    protected override void Dispose(bool disposing) { }
 }
