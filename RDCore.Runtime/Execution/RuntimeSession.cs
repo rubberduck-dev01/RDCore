835a976b59c70069012bfa71e59308b59f1aeafd
Mathieu Guindon <mathieu.guindon@dev.rubberduckvba.ca>
2026-08-16 01:02:22 -0400

    add session objects

--- RDCore.Runtime/Execution/RuntimeSession.cs
+++ RDCore.Runtime/Execution/RuntimeSession.cs
@@ -1,6 +1,8 @@
 ﻿using RDCore.Runtime.Execution.Memory;
+using RDCore.SDK.Model;
 using RDCore.SDK.Model.Symbols.Abstract;
 using RDCore.SDK.Model.Values.Bindings;
+using RDCore.SDK.Model.Values.Runtime;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("RDCore.Tests")]
@@ -13,45 +15,138 @@ public interface IRuntimeSession
     bool Is64Bit { get; }
     ISessionMemoryAllocator Memory { get; }
 }
-internal sealed class RuntimeSession(SessionMemory memory, SessionSymbols symbols) : IRuntimeSession
+internal sealed class RuntimeSession(ISessionMemoryAllocator memory, ISessionSymbols symbols, ISessionObjects objects) : IRuntimeSession
 {
     public bool Is64Bit { get; init; }
     public ISessionMemoryAllocator Memory { get; init; } = memory;
     public ISessionSymbols Symbols { get; init; } = symbols;
+    public ISessionObjects Objects { get; init; } = objects;
+}
+
+public interface ISessionObjects
+{
+    VBRuntimeObjectId CreateObject(Symbol symbol);
+    bool TryRemoveObject(VBRuntimeObjectId instance);
+    void AddRef(VBRuntimeObjectId instance, IBindingHandle handle);
+    int RemoveRef(VBRuntimeObjectId instance, IBindingHandle handle);
+}
+internal sealed class SessionObjects : ISessionObjects
+{
+    private readonly Dictionary<VBRuntimeObjectId, List<IBindingHandle>> _roots = [];
+    private readonly Dictionary<VBRuntimeObjectId, int> _refs = [];
+
+    public VBRuntimeObjectId CreateObject(Symbol symbol)
+    {
+        var id = new VBRuntimeObjectId();
+        _roots[id] = [];
+        _refs[id] = 0;
+        return id;
+    }
+
+    public void AddRef(VBRuntimeObjectId instance, IBindingHandle handle)
+    {
+        if (_roots.TryGetValue(instance, out var roots))
+        {
+            roots.Add(handle);
+        }
+        if (_refs.TryGetValue(instance, out var refs))
+        {
+            _refs[instance] = refs++;
+        }        
+    }
+
+    public int RemoveRef(VBRuntimeObjectId instance, IBindingHandle handle)
+    {
+        if (_roots.TryGetValue(instance, out var roots))
+        {
+            _ = roots.Remove(handle);
+        }
+        if (_refs.TryGetValue(instance, out var refs))
+        {
+            _refs[instance] = refs--;
+        }
+
+        return _refs[instance];
+    }
+
+    public bool TryRemoveObject(VBRuntimeObjectId instance)
+    {
+        if (_refs.TryGetValue(instance, out var refCount) && refCount == 0
+            /*&& _roots[instance].Count == 0*/)
+        {
+            return _refs.Remove(instance);
+        }
+        return false;
+    }
 }
 
 public interface ISessionSymbols
 {
-    bool TryDefine(Symbol symbol);
-    bool TryResolve(string name, Symbol scope, out Symbol symbol);
+    bool TryDefine(Symbol symbol, ScopeKind scope);
+    bool TryResolve(string name, Symbol scope, out Symbol? symbol);
 }
 internal sealed class SessionSymbols : ISessionSymbols
 {
-    private readonly Dictionary<Uri, Symbol> _symbolTable = [];
+    private readonly HashSet<Symbol> _globalSymbols = [];
+    private readonly HashSet<Symbol> _workspaceSymbols = [];
+    private readonly HashSet<Symbol> _staticLocalSymbols = [];
+
+    private readonly Dictionary<Uri, Symbol> _idMap = [];
     private readonly Dictionary<string, string> _nameTable = [];
 
-    public bool TryDefine(Symbol symbol)
+    public bool TryDefine(Symbol symbol, ScopeKind scope)
     {
-        // TODO
-        return false;
+        _nameTable[symbol.Name.ToLowerInvariant()] = symbol.Name;
+        var symbolTable = scope switch
+        {
+            ScopeKind.Global => _globalSymbols,
+            ScopeKind.Module => _workspaceSymbols,
+            ScopeKind.Instance => _staticLocalSymbols,
+            _ => default
+        };
+        return symbolTable?.Add(symbol) ?? false;
     }
-    public bool TryResolve(string name, Symbol scope, out Symbol symbol)
-    {
-        // TODO
-        symbol = default!;
-        return false;
-    }
-}
-internal sealed class SessionBindings
-{
-    private readonly Dictionary<Symbol, IBindingHandle> _globalSymbols = [];
-    private readonly Dictionary<Symbol, IBindingHandle> _workspaceSymbols = [];
-    private readonly Dictionary<Symbol, IBindingHandle> _staticLocalSymbols = []; // "static" in the VB sense here.
 
-    public bool TryGetValue(Symbol symbol, [MaybeNullWhen(false)][NotNullWhen(true)] out IBindingHandle handle)
+    private bool IsAccessibleFrom(Symbol symbol, Symbol scope)
     {
-        return _staticLocalSymbols.TryGetValue(symbol, out handle)
-            || _workspaceSymbols.TryGetValue(symbol, out handle)
-            || _globalSymbols.TryGetValue(symbol, out handle);
+        if (symbol.ParentUri == scope.Uri)
+        {
+            // local scope
+            return true;
+        }
+
+        var scopingParent = _idMap[scope.ParentUri];
+        if (scopingParent.Children.Any(c => c == symbol.Uri))
+        {
+            // same module
+            return true;
+        }
+
+        var scopingProject = _idMap[scopingParent.ParentUri];
+        if (scopingProject.Children.Any(c => c == symbol.Uri))
+        {
+            // same project
+            return ((AccessibleTypedSymbol)symbol).AccessModifier != AccessModifier.Private;
+        }
+
+        if (_globalSymbols.Contains(symbol))
+        {
+            return ((AccessibleTypedSymbol)symbol).AccessModifier != AccessModifier.Private;
+        }
+
+        return false;
     }
+
+    public bool TryResolve(string name, Symbol scope, out Symbol? symbol)
+    {
+        symbol = FindCandidates(name, _staticLocalSymbols).SingleOrDefault(s => s.ParentUri == scope.Uri)
+            ?? FindCandidates(name, _workspaceSymbols).OfType<AccessibleTypedSymbol>()
+                .SingleOrDefault(s => IsAccessibleFrom(s, scope))
+            ?? FindCandidates(name, _globalSymbols).OfType<AccessibleTypedSymbol>()
+                .FirstOrDefault(s => IsAccessibleFrom(s, scope));
+        return symbol != default;
+    }
+
+    private static Symbol[] FindCandidates(string name, HashSet<Symbol> symbolTable) 
+        => [.. symbolTable.Where(s => s.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase))];
 }
